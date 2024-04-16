3af86f39a8eb0b814d8d0fd1b2036e802929b2b5
wangxian <xian366@126.com>
2024-04-17 01:50:39 +0800

    update: startup log

--- startup.sh
+++ startup.sh
@@ -5,17 +5,18 @@ if [ ! -d "/run/mysqld" ]; then
 fi
 
 if [ -d /app/mysql/data ]; then
-  echo "[i] MySQL directory already present, skipping creation"
+  echo "---------------------- [i] MySQL directory already present, skipping creation"
 else
   mkdir -p /app/mysql/data
+  mkdir -p /app/mysql/data/mysql
   mkdir -p /app/mysql/log
-  echo "[i] MySQL data directory not found, creating initial databases"
+  echo "---------------------- [i] MySQL data directory not found, creating initial databases"
 
   mysql_install_db --user=root > /dev/null
 
   if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
     MYSQL_ROOT_PASSWORD=s6321..8
-    echo "[i] MySQL root Password: $MYSQL_ROOT_PASSWORD"
+    echo "---------------------- [i] MySQL root Password: $MYSQL_ROOT_PASSWORD"
   fi
 
   MYSQL_DATABASE=${MYSQL_DATABASE:-""}
@@ -36,6 +37,7 @@ ALTER USER 'root'@'localhost' IDENTIFIED BY '';
 EOF
 
   if [ "$MYSQL_DATABASE" != "" ]; then
+    echo "----------------------"
     echo "[i] Creating database: $MYSQL_DATABASE"
     echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;" >> $tfile
 
@@ -43,6 +45,7 @@ EOF
       echo "[i] Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
       echo "GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> $tfile
     fi
+    echo "----------------------"
   fi
 
   /usr/bin/mysqld --user=root --bootstrap --verbose=0 < $tfile
